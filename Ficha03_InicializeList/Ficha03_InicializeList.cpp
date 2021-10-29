886f54f5ee2992dc3f07b8ec6da815cb2e421f4a
goncalofcarnaz <46580022+goncalofcarnaz@users.noreply.github.com>
2021-10-29 18:58:55 +0100

    InicializeList

---
+++ Ficha03_InicializeList/Ficha03_InicializeList.cpp
@@ -0,0 +1,44 @@
+// Ficha03_InicializeList.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+#include <iostream>
+#include <vector>
+using namespace std;
+class MyNumber
+{
+public:
+    MyNumber(const initializer_list<int>& v) {
+        for (auto itm : v) {   // The auto keyword specifies that the type of the variable that is being declared will be automatically deducted from its initializer
+            mVec.push_back(itm);
+            
+        }
+    }
+
+    void print() {
+        for (auto itm : mVec) {
+            cout << itm << " " ;
+            
+        }
+        cout << "\n" << mVec.size();
+    }
+private:
+    vector<int> mVec;
+};
+
+int main()
+{
+    MyNumber m = { 1, 2, 3, 4,5,6 ,77,77 };
+    m.print();  // 1 2 3 4
+    
+    return 0;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
